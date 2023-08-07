61bfdb82e736827cecedc6f6733ef9c573119643
artu <esc.a.baibiene@gmail.com>
2023-08-07 16:22:16 -0300

    proyecto final angular

---
+++ src/app/services/box/box.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-box',
+  templateUrl: './box.component.html',
+  styleUrls: ['./box.component.css']
+})
+export class BoxComponent {
+  @Input('delay') delay: string = '';
+  @Input('titulo') titulo: string = '';
+  @Input('texto') texto: string = '';
+  @Input('icono') icono: string = '';
+  @Input('id') id: number = 0;
+
+  prueba = 'probando esto';
+
+  constructor(){  }
+
+  ngOnInit(): void {
+    // console.log(this.titulo, this.texto);
+  }
+
+
+
+}
